5c4e88cf2f5656c3f1004c10264ebb5228d477f6
unknown <zhonggy@firstshare.cn>
2016-08-04 15:36:21 +0800

    add tempPlugin

---
+++ SunXin/plugins/TempPlugin/src/main/java/com/sunxin/plugin/temp/RootFragment.java
@@ -0,0 +1,14 @@
+package com.sunxin.plugin.temp;
+// Copyright (c) 2016 ${ORGANIZATION_NAME}. All rights reserved.
+
+import android.support.v4.app.Fragment;
+
+import com.sunxin.lib.protobuf.TestProto;
+
+/**
+ * Created by 钟光燕 on 2016/8/4.
+ * e-mail guangyanzhong@163.com
+ */
+public class RootFragment extends Fragment {
+    TestProto proto  ;
+}
